8824f22ce09c7717c7deba89d849d90b100e8356
Mijoro Zo <mijoro.ei@gmail.com>
2025-12-06 16:30:16 +0300

    chore: initial Django backend setup (project + app + gitignore + readme)

---
+++ README.md
@@ -0,0 +1,8 @@
+# Codexia Backend – Deepfake Detector
+
+Backend Django REST Framework pour l’application mobile de détection de deepfake audio, image et vidéo.
+- Django
+- DRF
+- PostgreSQL
+- IA wav2vec2-base (audio)
+- Hébergement Google Cloud VM
---
+++ codexia_backend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for codexia_backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codexia_backend.settings')
+
+application = get_wsgi_application()
